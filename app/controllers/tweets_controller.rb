9c8b3a196f9e07c377aa0fb3ad4dfc4f39e4ba7d
hotate1000 <ooo.336.sss@gmail.com>
2020-09-06 23:30:04 +0900

    tweet new,createページ作成、バリデーション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,19 @@ class TweetsController < ApplicationController
   def index
     @tweets_index = Tweet.all
   end
+
+  def new
+    @tweets_new   = Tweet.new
+  end
+
+  def create
+    # binding.pry
+    Tweet.create(tweet_params)
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :text, :image)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :text,:image,:name, presence: true
 end
